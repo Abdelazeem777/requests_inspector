614fbc87092f7482e97611f0f7c6ab539f9dbc9c
Abdelrahman Tareq <abdotarekfathy@gmail.com>
2023-04-01 19:23:06 +0200

    feat: add GraphQLFlutterInterceptor

--- lib/requests_inspector.dart
+++ lib/requests_inspector.dart
@@ -7,3 +7,4 @@ export './src/requests_methods.dart';
 export './src/show_inspector_on_enum.dart';
 export './src/requests_inspector_interceptor.dart';
 export 'src/hasura_graph_qL_interceptor.dart';
+export 'src/graphql_flutter_interceptor.dart';
