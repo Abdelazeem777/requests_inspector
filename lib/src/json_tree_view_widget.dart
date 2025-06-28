f72b3d83d3c4302f4ad6a48192bfdec64a2dbd65
Abdelazeem Kuratem <abdelazeem263@gmail.com>
2025-06-28 19:48:45 +0300

    refactor: remove Provider dependency from JsonTreeView widget
    - Remove Consumer<InspectorController> usage throughout the widget
    - Pass isDarkMode as parameter instead of accessing through provider
    - Simplify widget structure and improve performance
    - Remove unnecessary provider import and context dependencies

--- lib/src/json_tree_view_widget.dart
+++ lib/src/json_tree_view_widget.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../requests_inspector.dart';
 
 class JsonTreeView extends StatelessWidget {
   final dynamic data;
-
-  const JsonTreeView(this.data, {super.key});
+  final bool _isDarkMode;
+  const JsonTreeView(this.data, {super.key, required bool isDarkMode})
+      : _isDarkMode = isDarkMode;
 
   @override
   Widget build(BuildContext context) {
@@ -43,9 +42,8 @@ class JsonTreeView extends StatelessWidget {
       return _buildLeafNode(context, keyName, '{}');
     }
 
-    return _buildExpandableTile(
-      context: context,
-      title: keyName != null ? '"$keyName" : ' : '',
+    return _CustomExpansionTile(
+      titleString: keyName != null ? '"$keyName" : ' : '',
       children: [
         ...map.entries.map((e) {
           return _buildNode(context, e.value, keyName: e.key);
@@ -55,6 +53,7 @@ class JsonTreeView extends StatelessWidget {
       collapsedCount: map.length,
       isObject: true,
       initiallyExpanded: true,
+      isDarkMode: _isDarkMode,
     );
   }
 
@@ -65,9 +64,8 @@ class JsonTreeView extends StatelessWidget {
       return _buildLeafNode(context, keyName, '[]');
     }
 
-    return _buildExpandableTile(
-      context: context,
-      title: keyName != null ? '"$keyName" : ' : '',
+    return _CustomExpansionTile(
+      titleString: keyName != null ? '"$keyName" : ' : '',
       children: [
         ...list.asMap().entries.map((e) {
           return _buildNode(context, e.value, keyName: null);
@@ -77,112 +75,84 @@ class JsonTreeView extends StatelessWidget {
       collapsedCount: list.length,
       isObject: false,
       initiallyExpanded: true,
+      isDarkMode: _isDarkMode,
     );
   }
 
   Widget _buildLeafNode(BuildContext context, String? key, dynamic value) {
-    return Consumer<InspectorController>(
-      builder: (context, controller, child) {
-        final isDarkMode = controller.isDarkMode;
+    String formattedValue;
+    Color valueColor;
 
-        String formattedValue;
-        Color valueColor;
+    if (value is String && (value == '{}' || value == '[]')) {
+      formattedValue = value;
+      valueColor = _isDarkMode ? Colors.white : Colors.black87;
+    } else if (value is String) {
+      formattedValue = '"$value"';
+      valueColor = _isDarkMode ? Colors.green.shade300 : Colors.green.shade700;
+    } else if (value is num) {
+      formattedValue = value.toString();
+      valueColor = _isDarkMode ? Colors.blue.shade300 : Colors.blue.shade700;
+    } else if (value is bool) {
+      formattedValue = value.toString();
+      valueColor =
+          _isDarkMode ? Colors.orange.shade300 : Colors.orange.shade700;
+    } else if (value == null) {
+      formattedValue = 'null';
+      valueColor = _isDarkMode ? Colors.grey.shade400 : Colors.grey.shade600;
+    } else {
+      formattedValue = value.toString();
+      valueColor = _isDarkMode ? Colors.white : Colors.black87;
+    }
 
-        if (value is String && (value == '{}' || value == '[]')) {
-          formattedValue = value;
-          valueColor = isDarkMode ? Colors.white : Colors.black87;
-        } else if (value is String) {
-          formattedValue = '"$value"';
-          valueColor =
-              isDarkMode ? Colors.green.shade300 : Colors.green.shade700;
-        } else if (value is num) {
-          formattedValue = value.toString();
-          valueColor = isDarkMode ? Colors.blue.shade300 : Colors.blue.shade700;
-        } else if (value is bool) {
-          formattedValue = value.toString();
-          valueColor =
-              isDarkMode ? Colors.orange.shade300 : Colors.orange.shade700;
-        } else if (value == null) {
-          formattedValue = 'null';
-          valueColor = isDarkMode ? Colors.grey.shade400 : Colors.grey.shade600;
-        } else {
-          formattedValue = value.toString();
-          valueColor = isDarkMode ? Colors.white : Colors.black87;
-        }
-
-        return Padding(
-          padding: const EdgeInsets.only(left: 12.0),
-          child: SelectableText.rich(
-            TextSpan(
-              children: [
-                if (key != null) ...[
-                  TextSpan(
-                    text: '"$key" : ',
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: isDarkMode ? Colors.white : Colors.black87,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ],
-                TextSpan(
-                  text: formattedValue,
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: valueColor,
-                    fontWeight: FontWeight.w500,
-                  ),
+    return Padding(
+      padding: const EdgeInsets.only(left: 12.0),
+      child: SelectableText.rich(
+        TextSpan(
+          children: [
+            if (key != null) ...[
+              TextSpan(
+                text: '"$key" : ',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: _isDarkMode ? Colors.white : Colors.black87,
+                  fontWeight: FontWeight.w500,
                 ),
-                if (key != null)
-                  TextSpan(
-                    text: ',',
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: isDarkMode ? Colors.white : Colors.black87,
-                    ),
-                  ),
-              ],
+              ),
+            ],
+            TextSpan(
+              text: formattedValue,
+              style: TextStyle(
+                fontSize: 14,
+                color: valueColor,
+                fontWeight: FontWeight.w500,
+              ),
             ),
-          ),
-        );
-      },
+            if (key != null)
+              TextSpan(
+                text: ',',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: _isDarkMode ? Colors.white : Colors.black87,
+                ),
+              ),
+          ],
+        ),
+      ),
     );
   }
 
   Widget _buildClosingBracket(BuildContext context, String bracket) {
-    return Consumer<InspectorController>(
-      builder: (context, controller, child) {
-        final isDarkMode = controller.isDarkMode;
-        return Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          // Consistent with general indentation
-          child: Text(
-            bracket,
-            style: TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.w500,
-              color: isDarkMode ? Colors.grey.shade400 : Colors.black87,
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  Widget _buildExpandableTile({
-    required BuildContext context,
-    required String title,
-    required List<Widget> children,
-    required bool initiallyExpanded,
-    int? collapsedCount,
-    bool isObject = false,
-  }) {
-    return _CustomExpansionTile(
-      titleString: title,
-      children: children,
-      initiallyExpanded: initiallyExpanded,
-      collapsedCount: collapsedCount,
-      isObject: isObject,
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0),
+      // Consistent with general indentation
+      child: Text(
+        bracket,
+        style: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.w500,
+          color: _isDarkMode ? Colors.grey.shade400 : Colors.black87,
+        ),
+      ),
     );
   }
 }
@@ -193,6 +163,7 @@ class _CustomExpansionTile extends StatefulWidget {
   final bool initiallyExpanded;
   final int? collapsedCount;
   final bool isObject;
+  final bool isDarkMode;
 
   const _CustomExpansionTile({
     required this.titleString,
@@ -200,6 +171,7 @@ class _CustomExpansionTile extends StatefulWidget {
     this.initiallyExpanded = false,
     this.collapsedCount,
     this.isObject = false,
+    required this.isDarkMode,
   });
 
   @override
@@ -218,115 +190,110 @@ class _CustomExpansionTileState extends State<_CustomExpansionTile>
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<InspectorController>(
-      builder: (context, controller, child) {
-        final isDarkMode = controller.isDarkMode;
-        final textColor = isDarkMode ? Colors.white : Colors.black87;
-        final secondaryTextColor =
-            isDarkMode ? Colors.grey.shade400 : Colors.grey.shade600;
+    final textColor = widget.isDarkMode ? Colors.white : Colors.black87;
+    final secondaryTextColor =
+        widget.isDarkMode ? Colors.grey.shade400 : Colors.grey.shade600;
 
-        final bool hasTitleString =
-            widget.titleString != null && widget.titleString!.isNotEmpty;
+    final bool hasTitleString =
+        widget.titleString != null && widget.titleString!.isNotEmpty;
 
-        return Padding(
-          padding: const EdgeInsets.only(left: 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              InkWell(
-                onTap: () {
-                  setState(() {
-                    _expanded = !_expanded;
-                  });
-                },
-                borderRadius: BorderRadius.circular(4),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 2),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      AnimatedRotation(
-                        turns: _expanded ? 0.25 : 0,
-                        duration: const Duration(milliseconds: 200),
-                        child: Icon(
-                          Icons.arrow_right,
-                          size: 16,
-                          color: secondaryTextColor,
-                        ),
-                      ),
-                      const SizedBox(width: 4),
-                      Flexible(
-                        fit: FlexFit.loose,
-                        child: Text.rich(
-                          TextSpan(
-                            children: [
-                              if (hasTitleString)
-                                TextSpan(
-                                  text: widget.titleString,
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 14,
-                                    color: textColor,
-                                  ),
-                                ),
-                              if (_expanded)
+    return Padding(
+      padding: const EdgeInsets.only(left: 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          InkWell(
+            onTap: () {
+              setState(() {
+                _expanded = !_expanded;
+              });
+            },
+            borderRadius: BorderRadius.circular(4),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 2),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  AnimatedRotation(
+                    turns: _expanded ? 0.25 : 0,
+                    duration: const Duration(milliseconds: 200),
+                    child: Icon(
+                      Icons.arrow_right,
+                      size: 16,
+                      color: secondaryTextColor,
+                    ),
+                  ),
+                  const SizedBox(width: 4),
+                  Flexible(
+                    fit: FlexFit.loose,
+                    child: Text.rich(
+                      TextSpan(
+                        children: [
+                          if (hasTitleString)
+                            TextSpan(
+                              text: widget.titleString,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 14,
+                                color: textColor,
+                              ),
+                            ),
+                          if (_expanded)
+                            TextSpan(
+                              text: widget.isObject ? '{' : '[',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 14,
+                                color: textColor,
+                              ),
+                            )
+                          else
+                            TextSpan(
+                              children: [
                                 TextSpan(
                                   text: widget.isObject ? '{' : '[',
                                   style: TextStyle(
-                                    fontWeight: FontWeight.w500,
                                     fontSize: 14,
                                     color: textColor,
+                                    fontWeight: FontWeight.w500,
                                   ),
-                                )
-                              else
-                                TextSpan(
-                                  children: [
-                                    TextSpan(
-                                      text: widget.isObject ? '{' : '[',
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        color: textColor,
-                                        fontWeight: FontWeight.w500,
-                                      ),
-                                    ),
-                                    if (widget.collapsedCount != null)
-                                      TextSpan(
-                                        text: widget.collapsedCount.toString(),
-                                        style: TextStyle(
-                                          fontSize: 14,
-                                          color: secondaryTextColor,
-                                        ),
-                                      ),
-                                    TextSpan(
-                                      text: widget.isObject ? '} ,' : '] ,',
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        color: textColor,
-                                        fontWeight: FontWeight.w500,
-                                      ),
-                                    ),
-                                  ],
                                 ),
-                            ],
-                          ),
-                        ),
+                                if (widget.collapsedCount != null)
+                                  TextSpan(
+                                    text: widget.collapsedCount.toString(),
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: secondaryTextColor,
+                                    ),
+                                  ),
+                                TextSpan(
+                                  text: widget.isObject ? '} ,' : '] ,',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: textColor,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ],
+                            ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
-                ),
+                ],
               ),
-              if (_expanded)
-                Padding(
-                  padding: const EdgeInsets.only(left: 12.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: widget.children,
-                  ),
-                ),
-            ],
+            ),
           ),
-        );
-      },
+          if (_expanded)
+            Padding(
+              padding: const EdgeInsets.only(left: 12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: widget.children,
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
